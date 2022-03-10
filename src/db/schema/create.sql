ff923e8cf23c17db75528707fdefd7965323fcf0
Adam Tyler <mr.adamtyler@gmail.com>
2022-03-10 15:20:30 -0800

    fill out create.sql

--- src/db/schema/create.sql
+++ src/db/schema/create.sql
@@ -1,6 +1,38 @@
--- DROP TABLE IF EXISTS table_name CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS prompts CASCADE;
+DROP TABLE IF EXISTS posts CASCADE;
+DROP TABLE IF EXISTS reaction_types CASCADE;
+DROP TABLE IF EXISTS users_posts_reactions CASCADE;
 
--- CREATE TABLE table_name (
---   column_name datatype PRIMARY KEY NOT NULL,
--- )
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  username varchar(3) NOT NULL ,
+  password varchar(30) NOT NULL
+);
 
+CREATE TABLE prompts (
+  id SERIAL PRIMARY KEY NOT NULL,
+  text varchar(255) NOT NULL
+);
+
+CREATE TABLE posts (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  prompt_id INTEGER REFERENCES prompts(id) ON DELETE CASCADE,
+  text varchar(255) NOT NULL,
+  creation_date DATE NOT NULL DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE reaction_types (
+  id SERIAL PRIMARY KEY NOT NULL,
+  index INTEGER NOT NULL DEFAULT 0,
+  label VARCHAR(20) NOT NULL,
+  icon VARCHAR(255) NOT NULL,
+);
+
+CREATE TABLE users_posts_reactions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  post_id INTEGER REFERENCES posts(id) ON DELETE CASCADE,
+  reaction_type_id INTEGER REFERENCES reaction_types(id) ON DELETE CASCADE
+)
