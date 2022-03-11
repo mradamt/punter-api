841e102ba71c9d421c7a597c4ded0450086d2a66
Adam Tyler <mr.adamtyler@gmail.com>
2022-03-11 15:56:08 -0800

    set up basic express app

--- src/app.js
+++ src/app.js
@@ -1,3 +1,15 @@
 // the express application
 // brings in db, routes, parsing & cors etc. protection
 
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Oh hi Mark')
+})
+
+app.listen(port, () => {
+  console.log(`app listening on port: ${port}`);
+})
