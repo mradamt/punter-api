14ad423c58bcb350ee0db29804d1704a9b16562e
Adam Tyler <mr.adamtyler@gmail.com>
2022-03-11 15:42:37 -0800

    init pg connection - Client for now

---
+++ src/db/index.js
@@ -0,0 +1,11 @@
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL || ""
+})
+
+client
+  .connect()
+  .catch(err => console.log(`Unable to connect to postgres server:\n${err}`))
+
+module.exports = client;
