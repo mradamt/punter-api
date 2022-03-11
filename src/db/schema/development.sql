a47af2f6d5792da7e0e64d369ff4acb078167f23
Adam Tyler <mr.adamtyler@gmail.com>
2022-03-10 17:24:23 -0800

    write schema development seeds

--- src/db/schema/development.sql
+++ src/db/schema/development.sql
@@ -16,3 +16,7 @@ INSERT INTO reaction_types (index, label, icon)
     (3, "genuine ha", "smiling-face-with-sunglasses_1f60e.png"),
     (4, "fave", "sports-medal_1f3c5.png");
 
+INSERT INTO posts (user_id, prompt_id, text, creation_date)
+  VALUES
+    (1, 1, "We'd be riding really fun ebikes made by Wheeebikes", "2022-01-01"),
+    (2, 1, "We'd go to Totes Boats for all our boating needs", "2022-01-02");
