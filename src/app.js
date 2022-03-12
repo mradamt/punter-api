56174da82ef7363ec9fe0b20e01bf953dd912b35
Adam Tyler <mr.adamtyler@gmail.com>
2022-03-11 16:28:40 -0800

    require() & install packages

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 // brings in db, routes, parsing & cors etc. protection
 
 const express = require('express');
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const helmet = require('helmet')
 
 const app = express();
+
+const db = require('./db');
+
+// const routes = require('./routes/')
 
 app.get('/', (req, res) => {
   res.send('Oh hi Mark')
 })
+
+function app () {
+  
+}
 
 module.exports = app;
