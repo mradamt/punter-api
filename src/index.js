313647a6363429257af1955051ed0c35d5c4485f
Adam Tyler <mr.adamtyler@gmail.com>
2022-03-11 16:02:41 -0800

    move code to now run basic app from index file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 // port, env, app, server, websockets
 
+const PORT = process.env.PORT || 8001;
+const ENV = require('./environment');
+
+const app = require('./app')
+
+app.listen(PORT, () => {
+  console.log(`app listening on port: ${PORT}`);
+})
