7bc08848a949aee77d4df98b70cb3f0a0da28771
Adam Tyler <mr.adamtyler@gmail.com>
2022-03-14 20:56:34 -0700

    write dbreset route
    using create.sql & development.sql to create & seed db
    use a readFile func that returns a promise

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 // brings in db, routes, parsing & cors etc. protection
+const fs = require('fs')
+const path = require('path')
 
 const express = require('express');
 const bodyParser = require('body-parser')
@@ -7,10 +9,35 @@ const helmet = require('helmet')
 
 const app = express();
 
-// const db = require('./db');
+const db = require('./db');
 
 // const routes = require('./routes/')
 
+// Readfile function to read in db reset instructions, rtn Promise
+const readFile = (filePath) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if(err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    })
+  })
+}
+
+app.get('/dbreset', (req, res) => {
+  Promise.all([
+    readFile(path.resolve(__dirname, './db/schema/create.sql')),
+    readFile(path.resolve(__dirname, './db/schema/development.sql'))
+  ])
+  .then(([create, seed]) => {
+    db.query(create);
+    db.query(seed);
+    // res.send(db.query('SELECT * from Users;'))
+  })
+  .catch(err => console.log(err))
+})
 app.get('/', (req, res) => {
   res.send('Oh hi Mark')
 })
