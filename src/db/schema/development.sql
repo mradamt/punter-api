5123453ce94e8674efaf9a694122a6bee87901fd
Adam Tyler <mr.adamtyler@gmail.com>
2022-03-15 18:41:40 -0700

    add some reactions to seed db data

--- src/db/schema/development.sql
+++ src/db/schema/development.sql
@@ -21,3 +21,12 @@ INSERT INTO posts (user_id, prompt_id, text, creation_date)
   VALUES
     (1, 1, 'We''d be riding really fun ebikes made by Wheeebikes', '2022-01-01 09:00:00-08'),
     (2, 1, 'We''d go to Totes Boats for all our boating needs', '2022-01-02 09:00:00-08');
+
+INSERT INTO users_posts_reactions (user_id, post_id, reaction_type_id)
+  VALUES
+    (1, 1, 2),
+    (1, 2, 2),
+    (2, 1, 3),
+    (2, 2, 3),
+    (3, 1, 4),
+    (3, 2, 4);
