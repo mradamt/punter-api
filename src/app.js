c0286b2918fb69d5688f99e9b58dc5a525e90420
Adam Tyler <mr.adamtyler@gmail.com>
2022-03-22 14:26:12 -0700

    add GET /user_reactions route

--- src/app.js
+++ src/app.js
@@ -122,6 +122,19 @@ app.get('/api/prompts', (req, res) => {
   })
 })
 
+// GET user's reactions
+app.get('/user_reactions/$userId', (req, res) => {
+  db.query(`
+    SELECT post_id, reaction_type_id
+    FROM users_posts_reactions
+    WHERE user_id = $1
+  `, [req.body.user_id])
+  .then(({rows: user_reactions}) => {
+    res.json(user_reactions)
+  })
+})
+
+
 app.get('/', (req, res) => {
   res.send('Oh hi Mark')
 })
